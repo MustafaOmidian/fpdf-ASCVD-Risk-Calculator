a189af87fce6adb705348b77aea57a411e044fdd
Mustafa Omidian <mustafaomidian@gmail.com>
2023-05-27 15:20:36 +0330

    upload jgraph and update

--- ascvd.php
+++ ascvd.php
@@ -1,56 +1,128 @@
 <?php
-function calculateASCVDScore($age, $gender, $totalCholesterol, $hdlCholesterol, $systolicBloodPressure, $isSmoker, $hasDiabetes) {
+require_once 'jpgraph/src/jpgraph.php';
+require_once 'jpgraph/src/jpgraph_bar.php';
+
+function calculateASCVDScore($age, $gender, $totalCholesterol, $hdlCholesterol, $systolicBloodPressure, $isSmoker, $hasDiabetes)
+{
+    if ($age < 40 || $age > 79) {
+        throw new InvalidArgumentException("ASCVD score calculator is applicable for individuals aged 40 to 79 only.");
+    }
+
     $score = 0;
-    if ($age >= 40 && $age <= 79) {
-        if ($gender === 'male') {
-            $score += 3;
-        }
 
-        if ($totalCholesterol >= 160 && $totalCholesterol <= 189) {
-            $score += 4;
-        } elseif ($totalCholesterol >= 190) {
-            $score += 8;
-        }
+    if ($gender === 'male') {
+        $score += 3;
+    }
 
-        if ($hdlCholesterol < 40) {
-            $score += 2;
-        } elseif ($hdlCholesterol >= 60) {
-            $score -= 1;
-        }
+    if ($totalCholesterol >= 160 && $totalCholesterol <= 189) {
+        $score += 4;
+    } elseif ($totalCholesterol >= 190) {
+        $score += 8;
+    }
 
-        if ($systolicBloodPressure >= 140 && $systolicBloodPressure <= 159) {
-            $score += 2;
-        } elseif ($systolicBloodPressure >= 160 && $systolicBloodPressure <= 179) {
-            $score += 3;
-        } elseif ($systolicBloodPressure >= 180) {
-            $score += 4;
-        }
+    if ($hdlCholesterol < 40) {
+        $score += 2;
+    } elseif ($hdlCholesterol >= 60) {
+        $score -= 1;
+    }
 
-        if ($isSmoker) {
-            $score += 2;
-        }
+    if ($systolicBloodPressure >= 140 && $systolicBloodPressure <= 159) {
+        $score += 2;
+    } elseif ($systolicBloodPressure >= 160 && $systolicBloodPressure <= 179) {
+        $score += 3;
+    } elseif ($systolicBloodPressure >= 180) {
+        $score += 4;
+    }
 
-        if ($hasDiabetes) {
-            $score += 4;
-        }
-    } else {
-        return "ASCVD score calculator is applicable for individuals aged 40 to 79 only.";
+    if ($isSmoker) {
+        $score += 2;
+    }
+
+    if ($hasDiabetes) {
+        $score += 4;
     }
 
     return $score;
 }
 
-// Example usage
+function calculateRiskPercentage($ascvdScore)
+{
+    $riskPercentages = [
+        0 => 5,
+         5 => 7,
+        10 => 11,
+        15 => 16,
+        20 => 22,
+        25 => 29,
+        30 => 36,
+        35 => 45,
+        40 => 54,
+        45 => 64,
+        50 => 75,
+    ];
 
-$age = $_POST['Age'];
-$gender = $_POST['Gender'];
-$totalCholesterol = $_POST['LDL'];
-$hdlCholesterol = $_POST['HDL'];
-$systolicBloodPressure = $_POST['SBP'];
-$isSmoker = $_POST['Smoker'];
-$hasDiabetes = $_POST['Diabetes'];
+    foreach ($riskPercentages as $scoreRange => $percentage) {
+        if ($ascvdScore >= $scoreRange) {
+            return $percentage;
+        }
+    }
 
-$ascvdScore = calculateASCVDScore($age, $gender, $totalCholesterol, $hdlCholesterol, $systolicBloodPressure, $isSmoker, $hasDiabetes);
+    return end($riskPercentages);
+}
 
-echo "ASCVD Score: " . $ascvdScore;
+try {
+    // Validate and sanitize input
+    $age = isset($_POST['Age']) ? intval($_POST['Age']) : 0;
+    $gender = isset($_POST['Gender']) ? htmlspecialchars($_POST['Gender']) : '';
+    $totalCholesterol = isset($_POST['LDL']) ? intval($_POST['LDL']) : 0;
+    $hdlCholesterol = isset($_POST['HDL']) ? intval($_POST['HDL']) : 0;
+    $systolicBloodPressure = isset($_POST['SBP']) ? intval($_POST['SBP']) : 0;
+    $isSmoker = isset($_POST['Smoker']) ? boolval($_POST['Smoker']) : false;
+    $hasDiabetes = isset($_POST['Diabetes']) ? boolval($_POST['Diabetes']) : false;
+
+    // Calculate ASCVD score
+    $ascvdScore = calculateASCVDScore($age, $gender, $totalCholesterol, $hdlCholesterol, $systolicBloodPressure, $isSmoker, $hasDiabetes);
+
+    // Output ASCVD score
+    echo "ASCVD Score: " . $ascvdScore . "<br>";
+
+    // Calculate risk percentage
+    $riskPercentage = calculateRiskPercentage($ascvdScore);
+
+    // Output risk percentage
+    echo "Risk Percentage: " . $riskPercentage . "<br>";
+
+    // Generate chart
+    $data = [
+        ['ASCVD Score', 'Risk Percentage'],
+        ['0-4', 5],
+        ['5-9', 7],
+        ['10-14', 11],
+        ['15-19', 16],
+        ['20-24', 22],
+        ['25-29', 29],
+        ['30-34', 36],
+        ['35-39', 45],
+        ['40-44', 54],
+        ['45-49', 64],
+        ['50+', 75]
+    ];
+
+    $labels = array_column($data, 0);
+    $percentages = array_column($data, 1);
+
+    $graph = new Graph(400, 300);
+    $graph->SetScale('textlin', 0, 80);
+    $graph->SetMargin(50, 30, 30, 50);
+    $graph->xaxis->SetTickLabels($labels);
+
+    $barplot = new BarPlot($percentages);
+    $barplot->SetFillColor('lightblue');
+    $graph->Add($barplot);
+
+    $chartFilename = 'chart.png';
+    $graph->Stroke($chartFilename);
+} catch (Exception $e) {
+    echo "An error occurred: " . $e->getMessage();
+}
 ?>
